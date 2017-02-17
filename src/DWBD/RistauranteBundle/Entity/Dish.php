fa4b3e943c549edd57ccec53755c07b2580fdc02
AntoninG <antonin.guilet-dupont@laposte.net>
2017-02-17 17:35:05 +0100

    SensioLabs Insight hints were shit

--- src/DWBD/RistauranteBundle/Entity/Dish.php
+++ src/DWBD/RistauranteBundle/Entity/Dish.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use DWBD\SecurityBundle\Entity\User;
 use DWBD\RistauranteBundle\Entity\Enum\StateEnum;
+use DWBD\RistauranteBundle\Entity\Enum\CategoryEnum;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
